f58289ef9a9d16984838d814be6ba43d493f6630
Sergey Kulbida <kulbida.sergey@outlook.com>
2023-11-13 19:36:58 +0700

    first code review

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# homework_bot
-python telegram bot
+# homework_bot - статус домашней работы
+
+---
+
+Телеграм бот для проверки статуса домашнего задания на курсе **Yandex.Practicum**.
+
+Работает на базе _**[«API сервиса Практикум.Домашка»](https://code.s3.yandex.net/backend-developer/%D0%9F%D1%80%D0%B0%D0%BA%D1%82%D0%B8%D0%BA%D1%83%D0%BC.%D0%94%D0%BE%D0%BC%D0%B0%D1%88%D0%BA%D0%B0%20%D0%A8%D0%BF%D0%B0%D1%80%D0%B3%D0%B0%D0%BB%D0%BA%D0%B0.pdf)**_
+
+## Установка
+
+---
+
+Для корректной работы необходимо:
+- _**[получить токен](https://oauth.yandex.ru/authorize?response_type=token&client_id=1d0b9dd4d652455a9eb710d450ff456a)**_ Яндекс-практикума;
+- _**[создать](https://t.me/BotFather)**_ Telegram-бота и получить его токен;
+- _**[получить ID](https://t.me/userinfobot)**_ своего аккаунта Telegram.
+
+
+  **`ВНИМАНИЕ:`НЕ ДОПУСКАЙТЕ РАСПРОСТРАНЕНЕИЯ СВОИХ ТОКЕНОВ!**
+
+
+1) Клонировать проект:
+```Bash
+git clone git@github.com:SergeyKDEV/homework_bot.git
+```
+
+2) Подготовить виртуальное окружение:
+
+```Bash
+# Установка виртуального окружения:
+
+# Для Linux/MacOS
+python3 -m venv venv
+
+# Для Windows
+python -m venv venv
+
+# Активация виртуального окружения:
+source venv/bin/activate
+
+# Обновление pip
+python -m pip install --upgrade pip 
+
+# Установка зависимостей
+pip install -r requirements.txt
+
+```
+3) Импортировать полученные токены в виртуальное окружение:
+- Для Linux/MacOS:
+```Bash
+export PRACTICUM_TOKEN=<PRACTICUM_TOKEN>
+export TELEGRAM_TOKEN=<TELEGRAM_TOKEN>
+export CHAT_ID=<CHAT_ID>
+```
+- _**[Инструкция для windows](https://www3.ntu.edu.sg/home/ehchua/programming/howto/Environment_Variables.html)**_
+
+  - Или создать файл `.env` в корне проекта, добавив в него токены. 
+
+4) Запустить проект:
+```Bash
+python homework.py
+```
+
+## Дополнительные настройки
+
+Переменная `RETRY_PERIOD` отвечает за частоту обновления статуса (в секундах).
+
+По умолчанию установлено значение 600 секунд.
+Может быть изменено при необходимости.
+
+---
+##
+- Автор: _**[Яндекс.Практикум](https://practicum.yandex.ru/)**_
+
+- Разработчик: _**[Кульбида Сергей](https://github.com/SergeyKDEV)**_
--- homework.py
+++ homework.py
@@ -1,65 +1,176 @@
-...
+import logging
+import os
+import time
+
+import requests
+import telegram
+import telegram.ext
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN: str = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN: str = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID: str = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_PERIOD = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+RETRY_PERIOD: int = 600
+ENDPOINT: str = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+HEADERS: dict = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
-
-HOMEWORK_VERDICTS = {
+HOMEWORK_VERDICTS: dict = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
 
-def check_tokens():
-    ...
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    '%(asctime)s [%(levelname)s] %(message)s'
+)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
-def send_message(bot, message):
-    ...
+class ApiAnswerNot200Error(Exception):
+    """Код ответа сервера API отличен от 200."""
 
 
-def get_api_answer(timestamp):
-    ...
+class RequestExceptionError(Exception):
+    """Ошибка запроса к серверу API."""
 
 
-def check_response(response):
-    ...
+class UnexpectedResponseError(Exception):
+    """Получен неверный формат ответа API."""
 
 
-def parse_status(homework):
-    ...
+class UnexpectedStatusError(Exception):
+    """Неожиданный статус домашней работы."""
 
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+
+def check_tokens() -> bool:
+    """Проверка переменных окружения."""
+    token_error_msg = "Отсутствует обязательная переменная окружения: '{}'"
+    flag: bool = True
+    if not PRACTICUM_TOKEN:
+        flag = False
+        logger.critical(token_error_msg.format('PRACTICUM_TOKEN'))
+    elif not TELEGRAM_TOKEN:
+        flag = False
+        logger.critical(token_error_msg.format('TELEGRAM_TOKEN'))
+    elif not TELEGRAM_CHAT_ID:
+        flag = False
+        logger.critical(token_error_msg.format('TELEGRAM_CHAT_ID'))
+    return flag
+
+
+def send_message(bot: telegram.Bot, message: str) -> None:
+    """Отправляет сообщение в Telegram чат."""
+    try:
+        bot.send_message(
+            chat_id=TELEGRAM_CHAT_ID,
+            text=message
+        )
+        logger.debug('Сообщение успешно отправлено')
+    except telegram.TelegramError as error:
+        logger.error(f'Ошибка отправки сообщения: {error}')
+
+
+def get_api_answer(timestamp: int) -> dict:
+    """Делает запрос к эндпойнту."""
+    # payload: dict = {'from_date': timestamp - 914_400 * CHECK_TIME_DAYS}
+    payload: dict = {'from_date': timestamp}
+    try:
+        response: requests.Response = requests.get(
+            url=ENDPOINT,
+            headers=HEADERS,
+            params=payload)
+        if response.status_code != 200:
+            endpoint_error_msg: str = (f'Сбой в работе программы: '
+                                       f'Эндпоинт {ENDPOINT} недоступен. '
+                                       f'Код ответа API: '
+                                       f'{response.status_code}')
+            logger.error(endpoint_error_msg)
+            raise ApiAnswerNot200Error(endpoint_error_msg)
+        return response.json()
+    except requests.exceptions.RequestException as request_exception:
+        get_api_answer_msg = (f'Ошибка доступа к API '
+                              f'(RequestException): {request_exception}.')
+        logger.error(get_api_answer_msg)
+        raise RequestExceptionError(get_api_answer_msg)
+
+
+def check_response(response: dict) -> bool:
+    """Выполняет валидацию ответа на соответствие документации."""
+    if type(response) is not dict:
+        type_error_msg = "Неожиданный тип данных переменной 'response'"
+        logger.error(type_error_msg)
+        raise TypeError(type_error_msg)
+    else:
+        homework: list = response.get('homeworks')
+
+    if type(homework) is not list:
+        type_error_msg = 'В ответе API получен неверный тип данных'
+        logger.error(type_error_msg)
+        raise TypeError(type_error_msg)
+    elif not homework:
+        type_error_msg = 'В ответе отсутствует ожидаемый ключ'
+        logger.error(type_error_msg)
+        raise UnexpectedResponseError(type_error_msg)
+    else:
+        return homework and response.get('current_date')
+
+
+def parse_status(homework: dict) -> str:
+    """Возвращает статус домашней работы."""
+    homework_name: str = homework.get('homework_name')
+    status: str = HOMEWORK_VERDICTS.get(homework.get('status'))
+    if not homework_name:
+        parse_status_msg = "В ответе отсутствует ключ 'homework_name'"
+        logger.error(parse_status_msg)
+        raise UnexpectedStatusError(parse_status_msg)
+    if not status:
+        parse_status_msg = "В ответе отсутствует ключ 'status'"
+        logger.error(parse_status_msg)
+        raise UnexpectedStatusError(parse_status_msg)
+    return f'Изменился статус проверки работы "{homework_name}". {status}'
 
 
 def main():
     """Основная логика работы бота."""
+    logger.debug('Запуск')
 
-    ...
+    if not check_tokens():
+        exit()
 
+    timestamp: int = int(time.time())
+    last_status: str = ''
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    timestamp = int(time.time())
-
-    ...
+    last_error: str = ''
 
     while True:
         try:
-
-            ...
-
+            response: dict = get_api_answer(timestamp)
+            if check_response(response):
+                last_homework: dict = response.get('homeworks')[0]
+                current_status: str = last_homework.get('status')
+                if last_status != current_status:
+                    message: str = parse_status(last_homework)
+                    send_message(bot, message)
+                    last_status = current_status
+                else:
+                    logger.debug('Статус домашней работы не изменился')
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+            error_msg = f'Сбой в работе программы: {error}'
+            logger.error(error_msg)
+            if error_msg != last_error:
+                send_message(bot, error_msg)
+                last_error = error_msg
+        finally:
+            time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
